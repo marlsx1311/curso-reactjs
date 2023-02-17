19668096285cc88b32f1b270e1d270cbb8489f1b
Marlon Luis Aponte Sierra <marlonaponte001@gmail.com>
2023-02-17 12:00:16 -0500

    actualizacion implementando css

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./TodoCounter.css"
 
 function TodoCounter(props){
     return (
-        <h2> has completado 2 de 3 TODOs</h2>
+        <h2> Has completado 2 de 3 TODOs</h2>
     );
 }
 export {TodoCounter};
