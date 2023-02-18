11891a7418274c9cb4a7c78077908d352b1c4b8b
Marlon Luis Aponte Sierra <marlonaponte001@gmail.com>
2023-02-17 22:18:00 -0500

    problema con el render de todos counter y el componente

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import { TodoList } from './TodoList';
 import { CreateTodoButton } from './CreateTodoButton';
 import { TodoItem } from './TodoItem';
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true},
   { text: 'Tomar el curso de react', completed: false},
   { text: 'llorar con la llorona', completed: false}
 ];
 
 function App() {
+  const[todos, setTodos]= React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length
+  
   return (
     <React.Fragment>
       <TodoCounter/>
-      <TodoSearch/>
+        total={totalTodos}
+        completed={completedTodos}
+        hola
+      <TodoSearch
+      searchValue ={searchValue}
+      setSearchValue ={setSearchValue}
+      />
       <TodoList>
         {todos.map(todo => (
           <TodoItem kye={todo.text} 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./TodoSearch.css"
 
-function TodoSearch(props){
-    const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({searchValue, setSearchValue}){
 
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
         setSearchValue(event.target.value);
     }
 
-    return [
+    return (
         <input placeholder='cebolla'
         onChange={onSearchValueChange}
         value={searchValue}
-        />,
-        <p>{searchValue}</p>
-    ];  
+        />
+        
+        );  
 }
 export {TodoSearch};
